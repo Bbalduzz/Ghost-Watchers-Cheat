f8f75ba1e10e9cb14bab1609de5b39f107e7eb2f
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-08-17 00:19:10 +0200

    Add files via upload

---
+++ Hacks.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Donteco;
+
+namespace GhostWatchersESP
+{
+    class Hacks : MonoBehaviour
+    {
+
+        public void OnGUI()
+        {
+            ghostESP();
+            playerESP();
+            itemESP();
+        }
+
+        public static void ghostESP()
+        {
+            foreach (Donteco.GhostAI ghost in FindObjectsOfType(typeof(Donteco.GhostAI)) as Donteco.GhostAI[])
+            {
+                Vector3 pivotPos = ghost.transform.position; //Pivot point NOT at the feet, at the center
+                Vector3 playerFootPos; playerFootPos.x = pivotPos.x; playerFootPos.z = pivotPos.z; playerFootPos.y = pivotPos.y - 2f; //At the feet
+                Vector3 playerHeadPos; playerHeadPos.x = pivotPos.x; playerHeadPos.z = pivotPos.z; playerHeadPos.y = pivotPos.y + 2f; //At the head
+
+                //Screen Position
+                Vector3 w2s_footpos = Camera.main.WorldToScreenPoint(playerFootPos);
+                Vector3 w2s_headpos = Camera.main.WorldToScreenPoint(playerHeadPos);
+
+                //Get ghost informations
+                string ghosttype = ghost.Data.name.Replace("(Clone)", "");
+                string ghostmood = ghost.Data.MoodProperties.ToString();
+                string ghostage = ghost.Data.AgeProperties.ToString();
+                float temp = ghost.Data.GetTemperatureValue();
+                int emp = ghost.Data.GetEmpValue();
+                float huntdistance = ghost.Data.DistanceForHunt;
+                bool cancapture = ghost.CanStartCapture();
+                bool canattack = ghost.CanStartAttack();
+                bool canrageattack = ghost.CanRangeAttack();
+                bool canhunt = ghost.CanHunt();
+                bool cancrattack = ghost.CanCriticalAttack();
+                bool isfullweak = ghost.IsFullWeakness.Value;
+                Render.GhostInfos(
+                    ghosttype,
+                    ghostage,
+                    ghostmood,
+                    temp,
+                    huntdistance,
+                    cancapture,
+                    canattack,
+                    canrageattack,
+                    canhunt,
+                    cancrattack,
+                    isfullweak,
+                    emp
+                );
+
+                if (w2s_footpos.z > 0f)
+                {
+                    // ghost esp
+                    Vector3 footpos = w2s_footpos;
+                    Vector3 headpos = w2s_headpos;
+                    float height = headpos.y - footpos.y;
+                    float widthOffset = 2f;
+                    float width = height / widthOffset;
+                    string entity = ghosttype;
+                    Color color = Color.white;
+                    foreach (Donteco.PlayerAnimation player in FindObjectsOfType(typeof(Donteco.PlayerAnimation)) as Donteco.PlayerAnimation[])
+                    {
+                        Render.DrawGhostBox(footpos.x - (width / 2), (float)Screen.height - footpos.y - height, width, height, color, 2f, entity, Math.Round(Vector3.Distance(player.transform.position, ghost.transform.position)));
+                    }      
+                    Render.DrawLine(new Vector2((float)(Screen.width / 2), (float)(Screen.height / 2)), new Vector2(footpos.x, (float)Screen.height - footpos.y), color, 1f);
+                }
+            }
+        }
+
+        public static void playerESP()
+        {
+            foreach (Donteco.PlayerAnimation player in FindObjectsOfType(typeof(Donteco.PlayerAnimation)) as Donteco.PlayerAnimation[])
+            {
+                //In-Game Position
+                Vector3 pivotPos = player.transform.position; //Pivot point NOT at the feet, at the center
+                Vector3 playerFootPos; playerFootPos.x = pivotPos.x; playerFootPos.z = pivotPos.z; playerFootPos.y = pivotPos.y - 2f; //At the feet
+                Vector3 playerHeadPos; playerHeadPos.x = pivotPos.x; playerHeadPos.z = pivotPos.z; playerHeadPos.y = pivotPos.y + 2f; //At the head
+
+                //Screen Position
+                Vector3 w2s_footpos = Camera.main.WorldToScreenPoint(playerFootPos);
+                Vector3 w2s_headpos = Camera.main.WorldToScreenPoint(playerHeadPos);
+
+                string player_nickname = player.name;
+                
+                if (w2s_footpos.z > 0f)
+                {
+                    // players esp
+                    Vector3 footpos = w2s_footpos;
+                    Vector3 headpos = w2s_headpos;
+                    float height = headpos.y - footpos.y;
+                    float widthOffset = 2f;
+                    float width = height / widthOffset;
+                    string entity = player_nickname;
+                    Color color = Color.blue;
+                    foreach (Donteco.GhostAI ghost in FindObjectsOfType(typeof(Donteco.GhostAI)) as Donteco.GhostAI[])
+                    {
+                        Render.DrawPlayerBox(footpos.x - (width / 2), (float)Screen.height - footpos.y - height, width, height, color, 1f, entity, Math.Round(Vector3.Distance(player.transform.position, ghost.transform.position)));
+                    }
+                }
+            }
+        }
+        public static List<ChildrenToyController> item;
+        private static void itemESP()
+        {
+           item = UnityEngine.GameObject.FindObjectsOfType<ChildrenToyController>().ToList();
+           foreach (Donteco.ChildrenToyController entity in item)
+            {
+                if (entity != null)
+                {
+                    if (IsOnScreen(entity.transform))
+                    {
+                        Vector3 w2s_obj = Camera.main.WorldToScreenPoint(entity.transform.position);
+                        Render.DrawString(new Vector2(w2s_obj.x, (float)Screen.height - w2s_obj.y), $"Cursed Item", Color.yellow);
+                    }
+                }
+            }
+        }
+
+        private static bool IsOnScreen(Transform ent_transform)
+        {
+            Vector3 w2s_check = Camera.main.WorldToScreenPoint(ent_transform.position);   
+            if (w2s_check.z > 0.1f && w2s_check.x > 0f && w2s_check.x < (float)Screen.width && w2s_check.y > 0 && w2s_check.y < (float)Screen.height) { return true;}
+            return false;
+        }
+
+        public void Update()
+        {
+            // to hide the esp (press "J")
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                Loader.Unload();
+            }
+        }
+    }
+}
---
+++ Render.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GhostWatchersESP
+{
+	public class Render : MonoBehaviour
+	{
+		public static GUIStyle StringStyle { get; set; } = new GUIStyle(GUI.skin.label);
+
+		public static Color Color
+		{
+			get { return GUI.color; }
+			set { GUI.color = value; }
+		}
+
+		public static void DrawString(Vector2 position, string label, Color color,  bool centered = true)
+		{
+			var content = new GUIContent(label);
+			var size = StringStyle.CalcSize(content);
+			var upperLeft = centered ? position - size / 2f : position;
+			GUI.Label(new Rect(upperLeft, size), content);
+		}
+
+		public static Texture2D lineTex;
+		public static void DrawLine(Vector2 pointA, Vector2 pointB, Color color, float width)
+		{
+			Matrix4x4 matrix = GUI.matrix;
+			if (!lineTex)
+				lineTex = new Texture2D(1, 1);
+
+			Color color2 = GUI.color;
+			GUI.color = color;
+			float num = Vector3.Angle(pointB - pointA, Vector2.right);
+
+			if (pointA.y > pointB.y)
+				num = -num;
+
+			GUIUtility.ScaleAroundPivot(new Vector2((pointB - pointA).magnitude, width), new Vector2(pointA.x, pointA.y + 0.5f));
+			GUIUtility.RotateAroundPivot(num, pointA);
+			GUI.DrawTexture(new Rect(pointA.x, pointA.y, 1f, 1f), lineTex);
+			GUI.matrix = matrix;
+			GUI.color = color2;
+		}
+
+		public static void DrawGhostBox(float x, float y, float w, float h, Color color, float thickness, string entity, double distance)
+		{
+			Vector2 P1 = new Vector2(x, y);
+			Vector2 P2 = new Vector2((x+(w/2))-90, y);
+			Vector2 P3 = new Vector2((x+(w/2))+90, y);
+			Vector2 P4 = new Vector2(x+w, y);
+			Vector2 P5 = new Vector2(x, (y+(h/2))-150);
+			Vector2 P6 = new Vector2(x, (y+(h/2))+150);
+			Vector2 P7 = new Vector2(x, y+h);
+			Vector2 P8 = new Vector2((x+(w/2))-90, y+h);
+			Vector2 P9 = new Vector2((x+(w/2))+90, y+h);
+			Vector2 P10 = new Vector2(x+w, y+h);
+			Vector2 P11 = new Vector2(x+w, (y+(h/2))+150); 
+			Vector2 P12 = new Vector2(x+w, (y+(h/2))-150);
+            DrawLine(P1, P2, color, thickness);
+			DrawLine(P1, P5, color, thickness);
+			DrawLine(P3, P4, color, thickness);
+			DrawLine(P4, P12, color, thickness);
+			DrawLine(P6, P7, color, thickness);
+			DrawLine(P7, P8, color, thickness);
+			DrawLine(P9, P10, color, thickness);
+			DrawLine(P10, P11, color, thickness);
+			GUI.Label(new Rect(x,y,w,h), $"{entity} - [{distance}]");
+		}
+
+		public static void DrawPlayerBox(float x, float y, float w, float h, Color color, float thickness, string entity, double distance)
+		{
+			DrawLine(new Vector2(x, y), new Vector2(x + w, y), color, thickness);
+			DrawLine(new Vector2(x, y), new Vector2(x, y + h), color, thickness);
+			DrawLine(new Vector2(x + w, y), new Vector2(x + w, y + h), color, thickness);
+			DrawLine(new Vector2(x, y + h), new Vector2(x + w, y + h), color, thickness);
+			GUI.Label(new Rect(x,y,w,h), $"{entity} - [{distance}]");
+		}
+
+		public static bool ShowMoreInfos = false;
+
+		public static void GhostInfos(string type, string age, string mood, float temp, float huntdistance, bool cancapture, bool canattack, bool canrageattack, bool canhunt, bool cancrattack, bool isfullweak, int emp_value)
+        {
+			GUI.Box(new Rect (10,10,200,150), "Ghost Infos");
+			string gtype = string.Format("Ghost Type: {0}", type);
+			GUI.Label(new Rect(20,40,200,20), new GUIContent(gtype));
+			string gage = string.Format("Ghost Age: {0}", age);
+			GUI.Label(new Rect(20,70,200,20), new GUIContent(gage));
+			string gmood = string.Format("Ghost Mood: {0}", mood);
+			GUI.Label(new Rect(20,100,200,20), new GUIContent(gmood));
+
+		    if (GUI.Button(new Rect(20,130,100,20), "Show More"))
+            ShowMoreInfos = !ShowMoreInfos;
+
+			if (ShowMoreInfos == true)
+            {
+				GUI.Box(new Rect (10,160,200,300), "Additional Infos");
+				string temps = string.Format("Temperature: {0}", temp);
+				GUI.Label(new Rect(20,190,200,20), new GUIContent(temps));
+				string emp = string.Format("Emp Level: {0}", emp_value);
+				GUI.Label(new Rect(20,220,200,20), new GUIContent(emp));
+				string hd = string.Format("Hunt Distance: {0}", huntdistance);
+				GUI.Label(new Rect(20,250,200,20), new GUIContent(hd));
+				string cc = string.Format("Can Capture Now: {0}", cancapture);
+				GUI.Label(new Rect(20,280,200,20), new GUIContent(cc));
+				string ca = string.Format("Can Attack Now: {0}", canattack);
+				GUI.Label(new Rect(20,310,200,20), new GUIContent(ca));
+				string cra = string.Format("Can Rage Attack Now: {0}", canrageattack);
+				GUI.Label(new Rect(20,340,200,20), new GUIContent(cra));
+				string ch = string.Format("Can Hunt Now: {0}", canhunt);
+				GUI.Label(new Rect(20,370,200,20), new GUIContent(ch));
+				string ccra = string.Format("Can Critical Hit: {0}", cancrattack);
+				GUI.Label(new Rect(20,400,200,20), new GUIContent(ccra));
+				string fw = string.Format("Full Weakness: {0}", isfullweak);
+				GUI.Label(new Rect(20,430,200,20), new GUIContent(fw));
+            }
+			
+        }
+    }
+}
+

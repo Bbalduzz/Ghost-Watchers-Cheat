18ab4cf8445c168b3e944cf09f422e287374767d
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-08-17 12:41:46 +0200

    Update Hacks.cs

--- Hacks.cs
+++ Hacks.cs
@@ -1,20 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using Donteco;
+using MhNetworking;
 
 namespace GhostWatchersESP
 {
     class Hacks : MonoBehaviour
     {
-
         public void OnGUI()
         {
+            Settings();
             ghostESP();
             playerESP();
             itemESP();
+            if(Render.AddMoneyAndEXP == true)
+            {
+                GiveMoneyAndExp(10000, 1000);
+            }
+            if(Render.UnlockAllAchievements == true)
+            {
+                UnlockAllAchievements();
+            }
+            if(Render.BecomeHost == true)
+            {
+                SetMasterClient();
+            }
+        }
+
+        public static void Settings()
+        {
+            Render.ShowESPSettings();
+            Render.ShowActions();
         }
 
         public static void ghostESP()
@@ -66,16 +85,26 @@ namespace GhostWatchersESP
                     float widthOffset = 2f;
                     float width = height / widthOffset;
                     string entity = ghosttype;
-                    Color color = Color.white;
+                    Color color = Color.red;
                     foreach (Donteco.PlayerAnimation player in FindObjectsOfType(typeof(Donteco.PlayerAnimation)) as Donteco.PlayerAnimation[])
                     {
-                        Render.DrawGhostBox(footpos.x - (width / 2), (float)Screen.height - footpos.y - height, width, height, color, 2f, entity, Math.Round(Vector3.Distance(player.transform.position, ghost.transform.position)));
+                        if (Render.ShowGhostESP == true)
+                        {
+                            Render.DrawGhostBox(footpos.x - (width / 2), (float)Screen.height - footpos.y - height, width, height, color, 2f, entity, Math.Round(Vector3.Distance(player.transform.position, ghost.transform.position)));
+                        }
                     }      
-                    Render.DrawLine(new Vector2((float)(Screen.width / 2), (float)(Screen.height / 2)), new Vector2(footpos.x, (float)Screen.height - footpos.y), color, 1f);
+                    if (Render.ShowGhostLine == true)
+                    {
+                        Render.DrawLine(new Vector2((float)(Screen.width / 2), (float)(Screen.height / 2)), new Vector2(footpos.x, (float)Screen.height - footpos.y), color, 1f);
+                    }
                 }
             }
         }
-
+        public static LobbyManager lobbyManager;
+        public static List<LobbyPlayer> lobbyPlayers;
+        public static Lobby lobby;
+        public static PlayerSetup localplayer;
+        public static List<PlayerSetup> network_player;
         public static void playerESP()
         {
             foreach (Donteco.PlayerAnimation player in FindObjectsOfType(typeof(Donteco.PlayerAnimation)) as Donteco.PlayerAnimation[])
@@ -89,8 +118,32 @@ namespace GhostWatchersESP
                 Vector3 w2s_footpos = Camera.main.WorldToScreenPoint(playerFootPos);
                 Vector3 w2s_headpos = Camera.main.WorldToScreenPoint(playerHeadPos);
 
-                string player_nickname = player.name;
-                
+                network_player = UnityEngine.GameObject.FindObjectsOfType<PlayerSetup>().ToList();
+                lobbyManager = LobbyManager.Instance;
+                lobby = LobbyManager.Current;
+                lobbyPlayers = lobby.Players.Values.ToList();
+                foreach (PlayerSetup p in network_player)
+                {
+                    if (p.IsLocalPlayer)
+                    {
+                        localplayer = p;
+                    }   
+                }
+                string nickname = "";
+                foreach (LobbyPlayer i in lobbyPlayers)
+                {
+                    foreach (PlayerSetup entity in network_player)
+                    {
+                         if (i.Id == entity.SteamId.ToString())
+                        {
+                            if (IsOnScreen(entity.transform))
+                            {
+                                nickname = $"{i.Nickname} [{Math.Round(Vector3.Distance(entity.transform.position, Hacks.localplayer.transform.position), 0)}]";
+                            }
+                        }
+                    }
+                   
+                }
                 if (w2s_footpos.z > 0f)
                 {
                     // players esp
@@ -99,11 +152,14 @@ namespace GhostWatchersESP
                     float height = headpos.y - footpos.y;
                     float widthOffset = 2f;
                     float width = height / widthOffset;
-                    string entity = player_nickname;
+                    string entity = nickname;
                     Color color = Color.blue;
                     foreach (Donteco.GhostAI ghost in FindObjectsOfType(typeof(Donteco.GhostAI)) as Donteco.GhostAI[])
                     {
-                        Render.DrawPlayerBox(footpos.x - (width / 2), (float)Screen.height - footpos.y - height, width, height, color, 1f, entity, Math.Round(Vector3.Distance(player.transform.position, ghost.transform.position)));
+                        if (Render.ShowPlayerESP == true)
+                        {
+                            Render.DrawPlayerBox(footpos.x - (width / 2), (float)Screen.height - footpos.y - height, width, height, color, 1f, entity, Math.Round(Vector3.Distance(player.transform.position, ghost.transform.position)));
+                        }
                     }
                 }
             }
@@ -130,6 +186,36 @@ namespace GhostWatchersESP
             Vector3 w2s_check = Camera.main.WorldToScreenPoint(ent_transform.position);   
             if (w2s_check.z > 0.1f && w2s_check.x > 0f && w2s_check.x < (float)Screen.width && w2s_check.y > 0 && w2s_check.y < (float)Screen.height) { return true;}
             return false;
+        }
+
+        public static PlayerWallet wallet;
+        public static void GiveMoneyAndExp(int money, int exp)
+        {
+            wallet = UnityEngine.GameObject.FindObjectOfType<PlayerWallet>();
+            if (wallet != null)
+            {
+                wallet.AddMoneyAndExp(money, exp);
+            }
+        }
+
+        public static void UnlockAllAchievements()
+        {
+            if (Render.UnlockAllAchievements == true)
+            {
+                foreach (Donteco.AchievementType a in Enum.GetValues(typeof(Donteco.AchievementType)))
+                {
+                    Donteco.AchievementsManager.IncrementAchievementValue(a);
+                }
+            }
+        }
+
+        public static void SetMasterClient()
+        {
+            GameObject localPlayer = Donteco.GameConsole.CommandUtils.FindLocalPlayer();
+            CommonMsg.SetMasterClient setMasterClient = new CommonMsg.SetMasterClient();
+            setMasterClient.ObjectId = -1;
+            setMasterClient.NewMasterId = localPlayer.GetComponent<NetIdentity>().PlayerId;
+            NetworkManager.Send((Message)setMasterClient);
         }
 
         public void Update()
